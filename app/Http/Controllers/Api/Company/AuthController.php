f157a9186ada17a0a3bdf135e5c05b3b94748c13
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2023-07-04 14:36:35 +0300

    test

--- app/Http/Controllers/Api/Company/AuthController.php
+++ app/Http/Controllers/Api/Company/AuthController.php
@@ -15,6 +15,7 @@ class AuthController extends Controller
     //
     use response,fileTrait;
     public function register(companyregister $request){
+        dd($request->logo);
        $logo=$this->MoveImage($request->logo,'uploads/companies/logo');
        $lang=$request->header('lang');
        $company=Company::create([
